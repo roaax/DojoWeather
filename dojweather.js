fc0d89afc114e574b3696f2e6feea4c527109510
Roaa Albishri <roaa20101@hotmail.com>
2022-05-30 21:20:53 +0300

    initial commit

---
+++ dojweather.js
@@ -0,0 +1,60 @@
+// var clickRemove = document.querySelector(".stop");
+// function remov(){
+//     console.log(clickRemove)
+//     clickRemove.remove();
+// }
+
+// function selectTemp(element){
+//     if(element= 'red'){
+
+//     }
+//     else{
+
+//     }
+// }
+
+
+// function getTemp(){
+//     document.getElementById('red');
+//     // .innerHTML=(showTemp.temp[showTemp.temp.selectedIndex].text
+// }
+function remov(e){ 
+    e.parentElement.remove(e);
+}
+
+
+// function changeTemp(e){
+//     console.log(e)
+//     e=e.querySelector('#celsius')
+
+//     if (e==e.querySelector('#celsius')){
+//         console.log("hi")
+//     }else {
+//         console.log('bay')
+//     }
+//     console.log();
+
+
+    // e.parentElement.parentElement.parentElement.queryselector('.num').innerText='t';
+// }
+
+// function cToF(celsius) 
+// {
+//   var cTemp = celsius;
+//   var cToFahr = cTemp * 9 / 5 + 32;
+//   var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+//     console.log(message);
+// }
+
+
+// function fToC(fahrenheit) 
+// {
+//   var fTemp = fahrenheit;
+//   var fToCel = (fTemp - 32) * 5 / 9;
+//   var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+//     console.log(message);
+// } 
+
+// function changeTemp(){
+
+// }
